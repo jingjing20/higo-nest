658219bbf559a78c1f505827ac26304da9203556
王志浩 <1269928993@qq.com>
2023-07-15 15:23:48 +0800

    feat: 定义获取当前用户的参数装饰器

--- src/auth/controllers/auth.controller.ts
+++ src/auth/controllers/auth.controller.ts
@@ -1,11 +1,13 @@
 import { Controller, Post, UseGuards } from '@nestjs/common';
 import { AuthLocalGuard } from '../modules/local/guards/auth-local.guard';
+import { CurrentUser } from '../decorators/current-user.decorator';
+import { UserEntity } from 'src/user/entities/user.entity';
 
 @Controller()
 export class AuthController {
   @Post('login')
   @UseGuards(AuthLocalGuard)
-  login() {
-    return 'logined in successfully';
+  login(@CurrentUser() user: UserEntity) {
+    return user;
   }
 }
